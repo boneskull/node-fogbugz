ccff675d453325eb54c9fd7344efb51c2d301c18
Christopher Hiller <chiller@badwing.com>
2014-07-06 00:06:45 -0700

    adding grunt-bump for my convenience

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,25 @@ module.exports = function (grunt) {
     pkg: require('./package.json'),
     jasmine_node: {
       projectRoot: "./spec"
+    },
+    bump: {
+      options: {
+        files: ['package.json'],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: false,
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+    }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.loadNpmTasks('grunt-bump');
 
   // Default task(s).
   grunt.registerTask('test', ['jasmine_node']);
